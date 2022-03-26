a2eab602ba4e84611db7aff65a6f59649e8bd28c
PietroBortolami <102406360+PietroBortolami@users.noreply.github.com>
2022-03-26 08:31:08 +0100

    Update index.php

--- dashboard/index.php
+++ dashboard/index.php
@@ -18,13 +18,14 @@ check_verified();
         <div class="col-sm-9">
 
             <div class="d-flex align-items-center p-3 mt-5 mb-3 text-white-50 bg-purple rounded box-shadow">
-                <img class="mr-3" src="../assets/images/logonotextwhite.png" alt="" width="48" height="48">
+                <img class="mr-3" src="../assets/images/Comma.png" alt="" width="48" height="48">
                 <div class="lh-100">
                     <h6 class="mb-0 text-white lh-100">Admin Dashboard</h6>
                     <small>[Development in Progress]</small>
                 </div>
             </div>
-
+			<div>ciao</div>
+			<div><img src="../assets/images/qualcosa.jfif"></div>
         </div>
     </div>
 </main>
@@ -32,8 +33,8 @@ check_verified();
 
 
 
-    <?php
+<?php
 
-    include '../assets/layouts/footer.php'
+include '../assets/layouts/footer.php'
 
-    ?>
+?>
